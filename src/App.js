5ed17b2cb7ace8a5372e96f4b0b2def10dac2422
Nicole Hollis <nicole@emmabeauty.co>
2019-09-20 08:39:33 -0700

    Characters rendered to screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./components/Header.js";
+import axios from "../node_modules/axios"
+import CharacterCard from "./components/CharacterCard"
+
 
 
 export default function App() {
+  const [characterInfo, setCharacterInfo] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://rickandmortyapi.com/api/character/`).then(response => {
+
+        const characterInfo = response.data.results;
+        console.log(response);
+        setCharacterInfo(characterInfo);
+       
+      })
+      .catch(error => {
+        console.log("API data was not returned", error);
+      });
+  }, []);
   return (
     <main>
       <Header />
-    </main>
-  );
+  
+
+<div>
+{characterInfo.map(info => {
+  console.log(info);
+  return (
+    <CharacterCard
+      key={info.index}
+      name={info.name}
+      species={info.species}
+      status={info.status}
+    />
+    )
+  })
+}
+</div>
+</main>
+  )
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  
+    console.log(props)
+  return <span key={props.index}>
+    <h1>{props.name}</h1>
+    <p>{props.species}</p>
+    <p>{props.status}</p>
+   
+  </span>;
 }
